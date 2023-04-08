50e65ac66c2c4ee101bb21f41eedeeeba188952c
WanjikuN <wanjiku@Wanjiku.localdomain>
2023-04-08 14:10:28 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
+let mongodb_url = 'mongodb+srv://Patricia:%23%211997%23%21@cluster0.3moce1s.mongodb.net/?retryWrites=true&w=majority';
+let dbName = 'Cluster0';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
 });
